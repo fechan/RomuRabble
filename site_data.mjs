bc5c788a7e6b2bcd456696626c786d38192a0610
fechan <edu.fechan@gmail.com>
2020-10-07 17:11:28 -0700

    add actual data

---
+++ site_data.mjs
@@ -0,0 +1,363 @@
+/**
+ * @fileoverview site_data.mjs exports JSON containing RomuRabble site data
+ */
+export const data = [
+  {
+    "season": 1,
+    "episode": 1,
+    "audio": "s1e1_discourse10_2443.571_[COMMUNICATION IN ROMULAN].mp3",
+    "spectrogram": "s1e1_discourse10_2443.571_[COMMUNICATION IN ROMULAN].png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 1,
+    "audio": "s1e1_discourse11_2551.461_[ANNOUNCEMENT IN ROMULAN].mp3",
+    "spectrogram": "s1e1_discourse11_2551.461_[ANNOUNCEMENT IN ROMULAN].png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 1,
+    "audio": "s1e1_discourse3_327.892_[IN ALIEN LANGUAGE] _.mp3",
+    "spectrogram": "s1e1_discourse3_327.892_[IN ALIEN LANGUAGE] _.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 2,
+    "audio": "s1e2_discourse3_1016.69_[SPEAKING ROMULAN] _.mp3",
+    "spectrogram": "s1e2_discourse3_1016.69_[SPEAKING ROMULAN] _.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 2,
+    "audio": "s1e2_discourse4_1927.482_[SPEAKING ROMULAN] _.mp3",
+    "spectrogram": "s1e2_discourse4_1927.482_[SPEAKING ROMULAN] _.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse10_1799.409_UNKNOWN.mp3",
+    "spectrogram": "s1e3_discourse10_1799.409_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse11_1835.292_(VOICES MURMURING QUIETLY IN ROMULAN).mp3",
+    "spectrogram": "s1e3_discourse11_1835.292_(VOICES MURMURING QUIETLY IN ROMULAN).png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse11_1843.55_UNKNOWN.mp3",
+    "spectrogram": "s1e3_discourse11_1843.55_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse3_696.577_UNKNOWN.mp3",
+    "spectrogram": "s1e3_discourse3_696.577_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse3_705.079_(FEMALE VOICE SPEAKING ROMULAN).mp3",
+    "spectrogram": "s1e3_discourse3_705.079_(FEMALE VOICE SPEAKING ROMULAN).png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse5_1034.033_(VOICES MURMURING IN ROMULAN).mp3",
+    "spectrogram": "s1e3_discourse5_1034.033_(VOICES MURMURING IN ROMULAN).png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse8_1568.567_(VOICES MURMURING QUIETLY IN ROMULAN).mp3",
+    "spectrogram": "s1e3_discourse8_1568.567_(VOICES MURMURING QUIETLY IN ROMULAN).png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse9_1602.546_UNKNOWN.mp3",
+    "spectrogram": "s1e3_discourse9_1602.546_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 3,
+    "audio": "s1e3_discourse9_1606.347_UNKNOWN.mp3",
+    "spectrogram": "s1e3_discourse9_1606.347_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse10_1348.539_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse10_1348.539_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse10_1350.74_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse10_1350.74_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse10_1352.341_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse10_1352.341_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse11_1889.276_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse11_1889.276_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse12_1914.78_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse12_1914.78_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse12_1917.581_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse12_1917.581_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse12_1919.049_[SPEAKING ROMULAN].mp3",
+    "spectrogram": "s1e4_discourse12_1919.049_[SPEAKING ROMULAN].png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse13_2024.913_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse13_2024.913_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse13_2026.715_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse13_2026.715_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse14_2114.064_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse14_2114.064_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse14_2119.765_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse14_2119.765_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse14_2124.166_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse14_2124.166_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse15_2252.917_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse15_2252.917_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse2_112.217_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse2_112.217_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse2_113.918_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse2_113.918_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse3_146.371_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse3_146.371_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse4_155.263_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse4_155.263_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_939.561_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_939.561_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_943.962_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_943.962_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_949.563_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_949.563_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_952.164_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_952.164_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_953.565_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_953.565_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_957.166_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_957.166_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_959.067_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_959.067_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse5_964.068_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse5_964.068_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse6_1079.996_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse6_1079.996_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse6_1097.396_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse6_1097.396_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 4,
+    "audio": "s1e4_discourse8_1172.313_UNKNOWN.mp3",
+    "spectrogram": "s1e4_discourse8_1172.313_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 6,
+    "audio": "s1e6_discourse2_2355.028_UNKNOWN.mp3",
+    "spectrogram": "s1e6_discourse2_2355.028_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 6,
+    "audio": "s1e6_discourse3_2357.565_UNKNOWN.mp3",
+    "spectrogram": "s1e6_discourse3_2357.565_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 6,
+    "audio": "s1e6_discourse3_2360.066_UNKNOWN.mp3",
+    "spectrogram": "s1e6_discourse3_2360.066_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse2_413.275_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse2_413.275_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse5_2276.713_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse5_2276.713_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse5_2278.514_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse5_2278.514_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse5_2280.715_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse5_2280.715_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse6_2311.627_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse6_2311.627_UNKNOWN.png",
+    "meaning": ""
+  },
+  {
+    "season": 1,
+    "episode": 7,
+    "audio": "s1e7_discourse6_2325.128_UNKNOWN.mp3",
+    "spectrogram": "s1e7_discourse6_2325.128_UNKNOWN.png",
+    "meaning": ""
+  }
+];
+export default data;
